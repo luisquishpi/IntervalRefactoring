176048ddb46e607551e585fd0922c656b0e9b1bf
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-29 03:55:14 +0200

    Update IntervalFactory

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -4,7 +4,6 @@ package intervals;
 public class IntervalFactory {
 
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
-		//TODO
-		return null;
+	    return new Interval(minimum, maximum,opening);
 	}
 }
