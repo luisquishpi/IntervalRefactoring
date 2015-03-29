f8d2bcf9825e3e77d4cd11a41c26c48cd14d87d3
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-29 03:39:23 +0200

    Init tests and intervals

---
+++ src/intervals/Interval.java
@@ -0,0 +1,41 @@
+package intervals;
+
+public class Interval {
+
+	public double midPoint() {
+		//TODO
+		return 0;
+	}
+
+	public boolean includes(double value) {
+		//TODO
+		return false;
+	}
+	
+	public boolean includes(Interval interval) {
+		//TODO
+		return false;
+	}
+	
+	public boolean intersectsWith(Interval interval) {
+		//TODO
+		return false;
+	}
+
+	public Interval intersection(Interval interval) {
+		//TODO
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		//TODO
+		return null;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		//TODO
+		return false;
+	}
+}
---
+++ src/intervals/IntervalFactory.java
@@ -0,0 +1,10 @@
+package intervals;
+
+
+public class IntervalFactory {
+
+	public static Interval getInterval(double minimum, double maximum, Opening opening) {
+		//TODO
+		return null;
+	}
+}
